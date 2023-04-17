7f60e61c1ccb65d2f863d2cfee55a6c111af290e
Sara <saraying03sb@gmail.com>
2023-04-17 17:27:16 +0200

    categories

---
+++ app/src/main/java/com/sara/tutorialrecycleview/CategoriesViewHolder.kt
@@ -0,0 +1,32 @@
+package com.sara.tutorialrecycleview
+
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    private val tvCategoryName: TextView = view.findViewById(R.id.tvCategoryName)
+    private val divider: View = view.findViewById(R.id.divider)
+
+    fun render(taskCategory: TaskCategory){
+        tvCategoryName.text = "EJEMPLO"
+        when(taskCategory){
+            TaskCategory.Business -> {
+
+                tvCategoryName.text = "Negocios"
+                divider.setBackgroundColor(ContextCompat.getColor(divider.context, R.color.business_category))
+            }
+            TaskCategory.Other -> {
+
+                tvCategoryName.text = "Otros"
+                divider.setBackgroundColor(ContextCompat.getColor(divider.context, R.color.other_category))
+            }
+            TaskCategory.Personal -> {
+
+                tvCategoryName.text = "Personal"
+                divider.setBackgroundColor(ContextCompat.getColor(divider.context, R.color.personal_category))
+            }
+        }
+    }
+}
--- app/src/main/java/com/sara/tutorialrecycleview/MainActivity.kt
+++ app/src/main/java/com/sara/tutorialrecycleview/MainActivity.kt
@@ -2,11 +2,39 @@ package com.sara.tutorialrecycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val categories = listOf(
+        TaskCategory.Business,
+        TaskCategory.Personal,
+        TaskCategory.Other
+
+    )
+
+    private lateinit var rvCategories:RecyclerView
+    private lateinit var  categoriesAdapter: CategoriesAdapter
+
+    private lateinit var  rvTasks: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initComponent()
+        initUI()
+    }
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
+    }
+
+    private fun initUI() {
+        categoriesAdapter = CategoriesAdapter(categories)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
     }
 }
